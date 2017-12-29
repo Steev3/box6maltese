29135333f42d1f03ba44845d03e0f1d24c3e7bfe
Steev3 <maltese.stefano7@gmail.com>
2017-12-29 21:18:14 +0100

    init box6maltese

--- js/init.js
+++ js/init.js
@@ -18,7 +18,10 @@ $(document).ready(function() {
 		$("#btn-contact").css("display","block");
 		$("#btn-contact-close").css("display","none");
 		$(".obscurator").fadeIn();
-		$("body").css("overflow-y","hidden")
+		$("body").css("overflow-y","hidden");
+		$(".flyout-content-mobile").css("height","0");
+		$(".services-on").css("display","block");
+		$(".services-off").css("display","none");
 	});
 	$("#btn-menu-close").click(function() {
 		$(".navbar-mobile").css("height","62px");
@@ -26,27 +29,45 @@ $(document).ready(function() {
 		$("#btn-menu-close").css("display","none");
 		$("#btn-menu").css("display","block");
 		$(".obscurator").fadeOut();
-		$("body").css("overflow-y","auto")
+		$("body").css("overflow-y","auto");
 	});
 });
 //Mobile menu activator
 
 //Mobile flyout activator
-
+$(document).ready(function() {
+	$(".services-on").click(function() {
+		$(".navbar-mobile").css("height","462px");
+		$("#ul-mobile").css("height","400px");
+		$(".flyout-content-mobile").css("height","100px");
+		$(".services-on").css("display","none");
+		$(".services-off").css("display","block");
+	});
+	$(".services-off").click(function() {
+		$(".navbar-mobile").css("height","362px");
+		$("#ul-mobile").css("height","300px");
+		$(".flyout-content-mobile").css("height","0");
+		$(".services-on").css("display","block");
+		$(".services-off").css("display","none");
+	});
+});
 //Mobile flyout activator
 
 //Mobile contacts activator
 $(document).ready(function() {
 	$("#btn-contact").click(function() {
-		$(".navbar-mobile").css("height","362px");
+		$(".navbar-mobile").css("height","462px");
 		$("#ul-mobile").css("height","0");
-		$("#ul-contact").css("height","300px");
+		$("#ul-contact").css("height","400px");
 		$("#btn-contact-close").css("display","block");
 		$("#btn-contact").css("display","none");
 		$("#btn-menu").css("display","block");
 		$("#btn-menu-close").css("display","none");
 		$(".obscurator").fadeIn();
-		$("body").css("overflow-y","hidden")
+		$("body").css("overflow-y","hidden");
+		$(".flyout-content-mobile").css("height","0");
+		$(".services-on").css("display","block");
+		$(".services-off").css("display","none");
 	});
 	$("#btn-contact-close").click(function() {
 		$(".navbar-mobile").css("height","62px");
@@ -54,7 +75,7 @@ $(document).ready(function() {
 		$("#btn-contact-close").css("display","none");
 		$("#btn-contact").css("display","block");
 		$(".obscurator").fadeOut();
-		$("body").css("overflow-y","auto")
+		$("body").css("overflow-y","auto");
 	});
 });
 //Mobile contacts activator
@@ -70,7 +91,10 @@ $(document).ready(function() {
 		$("#btn-menu-close").css("display","none");
 		$("#btn-contact-close").css("display","none");
 		$(".obscurator").fadeOut();
-		$("body").css("overflow-y","auto")
+		$("body").css("overflow-y","auto");
+		$(".flyout-content-mobile").css("height","0");
+		$(".services-on").css("display","block");
+		$(".services-off").css("display","none");
 	});
 	$(".obscurator").click(function() {
 		$(".navbar-mobile").css("height","62px");
@@ -81,7 +105,10 @@ $(document).ready(function() {
 		$("#btn-menu-close").css("display","none");
 		$("#btn-contact-close").css("display","none");
 		$(".obscurator").fadeOut();
-		$("body").css("overflow-y","auto")
+		$("body").css("overflow-y","auto");
+		$(".flyout-content-mobile").css("height","0");
+		$(".services-on").css("display","block");
+		$(".services-off").css("display","none");
 	});
 });
 //Brandlogo e obscurator close menu
